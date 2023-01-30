1dc85b7935e1684b9f52f8550477c44ff614c5a7
Prashant Chavan <prashantch451@outlook.com>
2023-01-30 20:51:14 +0530

    proudct schema updated to work with uploading multiple images for the server

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,15 +1,98 @@
 
 const Products = require('../models/product.model')
-const express = require("express");
+const express = require('express');
+const app = express();
 const router = express.Router();
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
-router.post('/', async (req,res)=>{
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use("/uploads", express.static("uploads"));
+
+// Creating uploads folder if not already present
+// In "uploads" folder we will temporarily upload
+// image before uploading to cloudinary
+if (!fs.existsSync("./uploads")) {
+    fs.mkdirSync("./uploads");
+}
+
+// Multer setup
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+
+var upload = multer({ storage: storage });
+
+cloudinary.config({
+    cloud_name: "dkdovv1mp",
+    api_key: "689329276849189",
+    api_secret: "NAySxZhmVYt9l8veK_SXshI-HCU",
+});
+
+async function uploadToCloudinary(locaFilePath) {
+  
+    // locaFilePath: path of image which was just
+    // uploaded to "uploads" folder
+  
+    var mainFolderName = "main";
+    // filePathOnCloudinary: path of image we want
+    // to set when it is uploaded to cloudinary
+    var filePathOnCloudinary = 
+        mainFolderName + "/" + locaFilePath;
+  
+    return cloudinary.uploader
+        .upload(locaFilePath, { public_id: filePathOnCloudinary })
+        .then((result) => {
+  
+            // Image has been successfully uploaded on
+            // cloudinary So we dont need local image 
+            // file anymore
+            // Remove file from local uploads folder
+            fs.unlinkSync(locaFilePath);
+  
+            return {
+                message: "Success",
+                url: result.url,
+            };
+        })
+        .catch((error) => {
+  
+            // Remove file from local uploads folder
+            fs.unlinkSync(locaFilePath);
+            return { message: "Fail" };
+        });
+}
+
+router.post('/', upload.array("product-images", 12), async (req,res)=>{
+
+    let imageUrlList = [];
+
+    for (var i = 0; i < req.files.length; i++) {
+        var locaFilePath = req.files[i].path;
+
+        // Upload the local image to Cloudinary
+        // and get image url as response
+        var result = await uploadToCloudinary(locaFilePath);
+        imageUrlList.push(result.url);
+    }
+
     try {
         const product = await Products.create({
             productName : req.body.productName,
             description: req.body.description,
             price: req.body.price,
             discountPrice: req.body.discountPrice,
+            images: imageUrlList
         })
         res.json({status : 'ok'})
     } catch (error) {
