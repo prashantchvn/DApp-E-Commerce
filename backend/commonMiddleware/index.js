1a4f0a94220690ccfc508ed0a93114666c872371
Prashant Chavan <prashantch451@outlook.com>
2023-02-01 00:08:37 +0530

    authorization secret key is added to the common middleware which is checking every request is authorized or not

--- backend/commonMiddleware/index.js
+++ backend/commonMiddleware/index.js
@@ -1,7 +1,9 @@
+const jwt = require("jsonwebtoken");
+
 exports.requireSignin = (req, res, next) => {
     if (req.headers.authorization) {
       const token = req.headers.authorization.split(" ")[1];
-      const user = jwt.verify(token, process.env.JWT_SECRET);
+      const user = jwt.verify(token,"alksdjlskjfskldfjsdlkjfs");
       req.user = user;
     } else {
       return res.status(400).json({ message: "Authorization required" });
