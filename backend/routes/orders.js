8630593058326485b553fd176572fce1314d0f59
Prashant Chavan <prashantch451@outlook.com>
2023-02-02 22:02:13 +0530

    suggested changes for the backend route and fixed the admin route for the orders

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -12,18 +12,15 @@ router.post('/', async (req, res) => {
     // get the list of orders placed logged in user
     try {
         // if the cart is empty
-        let cartLength = await cartItems(req.user)
-        if (cartLength.length == 0) {
+        let cartItem = await cartItems(req.user)
+        if (cartItem.length == 0) {
             res.json({ status: 'ok', message: "Order cannot be placed for empty cart" })
             return
         }
-        // if (await cartItems(req.user).length === 0) {
-        //     res.json({ status: 'or', message: "Cart is empty so order cannot be placed" })
-        // }
         let order = await Orders.findOne({ orderPlacedBy: req.user })
         if (order) {
 
-            // if first order placed by an user then create new record for the new user
+            // if first order placed by new user then create new record for the new user
             order = await Orders.findOneAndUpdate({ orderPlacedBy: req.user }, {
                 $push: {
                     orders: {
@@ -36,7 +33,7 @@ router.post('/', async (req, res) => {
                 }
             })
         } else {
-            // if user already have an record for his user Id then push the order into orders array
+            // if user already have an record for current user Id then push the order into orders array
             order = await Orders.create({
                 orderPlacedBy: req.user,
                 orders: {
@@ -56,7 +53,7 @@ router.post('/', async (req, res) => {
                 }
             })
         })
-        const data = await Orders.find({ orderPlacedBy: req.user }).populate("orderPlacedBy").populate("orders.cartItems.product")
+        const data = await Orders.find({ orderPlacedBy: req.user }).populate("orders.cartItems.product")
         res.json({ status: 'ok', data: data })
     } catch (error) {
         console.log(error)
@@ -72,7 +69,7 @@ router.get('/', async (req, res) => {
             const data = await Orders.findOne({ orderPlacedBy: req.user }).populate("orders.cartItems.product")
             res.json({ status: 'ok', data: data.orders })
         } else {
-            res.json({ status: 'error', message: "No orders found for relative user" })
+            res.json({ status: 'error', data: [], message: "No orders found for relative user" })
         }
     } catch (error) {
         console.log(error)
@@ -85,7 +82,7 @@ router.get('/admin', async (req, res) => {
     // get the list of orders placed by all users Items
     // admin route
     try {
-        const data = await Orders.find({}).populate("orderPlacedBy").populate("cartItems")
+        const data = await Orders.findOne({}).populate("orders.cartItems.product").populate("orderPlacedBy")
         res.json({ status: 'ok', data: data })
     } catch (error) {
         res.json({ status: 'error', error: error })
