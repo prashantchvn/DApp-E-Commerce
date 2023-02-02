4fb3bcab3a99e36910a9ce28c4cc644f53354d33
Prashant Chavan <prashantch451@outlook.com>
2023-02-02 15:38:58 +0530

    order schema defined for the orders model

---
+++ backend/models/orders.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    orderPlacedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'UserData', required: true },
+    cartItems: { type: mongoose.Schema.Types.ObjectId, ref: 'Cart', required: true },
+    OrderTotal: { type: Number, required: true },
+    paymentMode: {type: String, required: true },
+    paymentReference: {type: String, required: true },
+    paidStatus: {type: Boolean, default: false},
+    orderPlaced: {type: Boolean, default: false},
+    deliveryAgency: {type: String, required: true }
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('Orders', cartSchema);
