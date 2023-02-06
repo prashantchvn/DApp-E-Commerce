0e443ba7848ae5c364b3e0be0b9a18f7a57679e3
Prashant Chavan <prashantch451@outlook.com>
2023-02-06 16:17:52 +0530

    product schema updated to work with categories

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose')
 
 const ProductSchema = new mongoose.Schema({
-        productName: { type: 'String', required: true },
-        description: { type: 'String' },
-        slug: {type: 'String', required: true, unique: true },
-        price: { type: 'Number', required: true },
-        discountPrice: { type: 'Number', required: true },
-        images:  [{
-            type: String
-        }]
-    }, 
+    productName: { type: 'String', required: true },
+    description: { type: 'String' },
+    category: { type: 'String' },
+    slug: { type: 'String', required: true, unique: true },
+    price: { type: 'Number', required: true },
+    discountPrice: { type: 'Number', required: true },
+    images: [{
+        type: String
+    }]
+},
     {
         collection: 'products-data'
     }
 )
 
-const model = mongoose.model('ProductData',ProductSchema)
+const model = mongoose.model('ProductData', ProductSchema)
 
 module.exports = model
