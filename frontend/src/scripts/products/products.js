ddd315b821dc64c75ef5a307a76e093eea9a0a2e
Prashant Chavan <prashantch451@outlook.com>
2023-03-20 17:44:47 +0530

    product related api call added in frontend

--- frontend/src/scripts/products/products.js
+++ frontend/src/scripts/products/products.js
@@ -1,4 +1,4 @@
-import { Get } from "../apiClient";
+import { Get, Post, Patch, Delete } from "../apiClient";
 
 export function getProducts(){
     return Get('/api/products')
@@ -6,4 +6,16 @@ export function getProducts(){
 
 export function getProduct(slug){
     return Get(`/api/products/${slug}`)
+}
+
+export function createProduct(data){
+    return Post('/api/products',data);
+}
+
+export function updateProduct(slug, data){
+    return Patch(`/api/products/update/${slug}`,data);
+}
+
+export function deleteProduct(slug){
+    return Delete(`/api/products/delete/${slug}`);
 }
